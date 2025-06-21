34ab9dbad17ef3167fd3277bc990e5340fd8a8da
RafaelBrantNobre <rafael.rbn.gb@gmail.com>
2025-06-21 02:36:19 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/plataforma-orientacao/', // 👈 ESSENCIAL para GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
